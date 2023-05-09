6a6e156722dd5d8107cf1199b309fe243fb586f7
I-Maged <magedhanafy@gmail.com>
2023-05-09 09:44:07 +0300

    entry service

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import entriesReducer from '../features/entries/entriesSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    entries: entriesReducer,
   },
 });
